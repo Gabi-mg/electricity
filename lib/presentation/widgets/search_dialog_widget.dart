7e2a8c0df634450a41ebc6ad357d238769b3ed4f
Gabriel Muñoz García <gabriel.munoz.garcia@accenture.com>
2022-06-03 12:23:43 +0200

    search dialog is refactored

---
+++ lib/presentation/widgets/search_dialog_widget.dart
@@ -0,0 +1,39 @@
+import 'package:electricity/presentation/screens/home/bloc/home_bloc.dart';
+import 'package:electricity/presentation/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SearchDialogWidget extends StatelessWidget {
+  const SearchDialogWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      elevation: 5,
+      title: const Text('Elija criterios de búsqueda'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: const [
+          CustomDropdownWidget(),
+          InputDatepickerWidget(),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            final priceBlocProvider = BlocProvider.of<HomeBloc>(
+              context,
+              listen: false,
+            );
+            priceBlocProvider.add(const LoadingPriceEvent());
+            Navigator.pop(context);
+          },
+          child: const Text('Aceptar'),
+        ),
+      ],
+    );
+  }
+}
